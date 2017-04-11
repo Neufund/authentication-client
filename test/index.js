4232e7d72e2acf136662236f7504733f3d0ce24a
Leonid Logvinov <logvinov.leon@gmail.com>
2017-04-11 17:11:56 +0200

    Add tests for srpLogin and srpRegister

--- test/index.js
+++ test/index.js
@@ -2,19 +2,48 @@
 /* eslint-disable no-underscore-dangle */
 import 'babel-polyfill';
 import chai from 'chai';
+import dirtyChai from 'dirty-chai';
+import faker from 'faker';
 import Authenticator from '../src/index';
 
 const expect = chai.expect;
+chai.use(dirtyChai);
+
+const API_URL = 'fake url';
 
 describe('Authenticator', () => {
   describe('#_srpCheckServer', () => {
 
   });
   describe('#_srpLogin', () => {
+    const reverse = str => str.split('').reverse().join(''); // Tech interviews made me to remember this line at 4am
 
+    it('should return clientPublicKey and clientProof', async () => {
+      const authenticator = new Authenticator(API_URL);
+      const email = faker.internet.email();
+      const password = faker.internet.password();
+      const kdfSalt = Authenticator._generateSalt();
+      const key = await Authenticator._scrypt(password, kdfSalt);
+      const { salt } = await authenticator._srpRegister(email, key);
+      const serverPublicKey = reverse(authenticator.srp.getPublicKey());
+      const { clientPublicKey, clientProof } =
+        await authenticator._srpLogin(email, key, salt, serverPublicKey);
+      expect(clientPublicKey).to.be.a('string');
+      expect(clientProof).to.be.a('string');
+    });
   });
-  describe('#_srpRegister', () => {
-
+  describe('#_srpRegister', async () => {
+    it('should return salt and verifier', async () => {
+      const authenticator = new Authenticator(API_URL);
+      const email = faker.internet.email();
+      const password = faker.internet.password();
+      const kdfSalt = Authenticator._generateSalt();
+      const key = await Authenticator._scrypt(password, kdfSalt);
+      const { salt, verifier } = await authenticator._srpRegister(email, key);
+      expect(salt).to.have.lengthOf(64);
+      expect(salt).to.not.equal(kdfSalt);
+      expect(verifier).to.be.a('string');
+    });
   });
   describe('#_scrypt', () => {
     const hex = (str) => {
